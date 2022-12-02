4ff03f9103044c1d1d600fdf0b854c79601f06a4
Johandros <hansjuergoe@gmail.com>
2022-12-02 01:24:36 +0100

    ignore inline goto without actual class func

--- Classner.py
+++ Classner.py
@@ -56,7 +56,7 @@ class Classner:
                             if len(mo.group(0)) > 0:
                                 class_functions.append([line]) # probably correct function name, without return type
                                 next_is_decl = True
-                elif "_Z" in line:
+                elif "_Z" in line and "(" in line and ")" in line:
                     regex = re.compile(r'(_Z[a-zA-Z0-9-_]+\([a-zA-Z,.<>:0-9-_& \*]*\))')
                     mo = regex.search(line)
                     # FIXME: a lot of classes and their functions are still not included
