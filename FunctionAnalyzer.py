d447a239f931adf7616537644eacf4bb61a3524d
Johandros <hansjuergoe@gmail.com>
2022-12-05 03:45:36 +0100

    analyzer fixed_classes ignored fix

--- FunctionAnalyzer.py
+++ FunctionAnalyzer.py
@@ -9,7 +9,7 @@ class FunctionAnalyzer:
         pass
 
 
-    def _help_find_original_class(self, classes, fixed_classes, partNum, maxPartCount):
+    def _help_find_original_class(self, classes, partNum, maxPartCount):
         packSize = int(len(classes) / maxPartCount)
         startIndex = int(packSize * partNum)
         endIndex = startIndex + packSize
@@ -17,17 +17,17 @@ class FunctionAnalyzer:
             endIndex = len(classes.keys())
         keyList = list(classes.keys())[startIndex:endIndex]
         for cls in keyList:
-            self._findOriginalClassS(cls, classes, fixed_classes)
+            self._findOriginalClassS(cls, classes)
         #print("Process", partNum, "finished!")
 
 
-    def _findOriginalClassS(self, cls, classes, fixed_classes):
+    def _findOriginalClassS(self, cls, classes):
         print("Analyzing", cls, "...", end="", flush=True)
         tracerx = dict()
-        fixed_classes[cls] = dict()
+        self.fixed_classes[cls] = dict()
         #found_something = False
         for func in classes[cls]:
-            fixed_classes[cls][func] = []
+            self.fixed_classes[cls][func] = []
             for line in classes[cls][func]:
                 for cls2 in classes:
                     for func2 in classes[cls2]:
@@ -57,13 +57,13 @@ class FunctionAnalyzer:
                                 else:
                                     print(cls2, ">", "ERROR: EMPTY RDI_OBJECT!", line.rstrip("\n")) # FIXME: fix this in original generation of these rdi->func() lines
 
-                fixed_classes[cls][func].append(line)
+                self.fixed_classes[cls][func].append(line)
         print("DONE")
 
         print("Fixing", cls, "...", end="", flush=True)
-        for func in fixed_classes[cls]:
+        for func in self.fixed_classes[cls]:
             #found_something = False
-            for i, line in enumerate(fixed_classes[cls][func]):
+            for i, line in enumerate(self.fixed_classes[cls][func]):
                 for trace in tracerx:
                     if " " + trace + ";" in line:
                         linx = line.strip().split(";")[0]
@@ -76,7 +76,7 @@ class FunctionAnalyzer:
                             if "STRUCT_" in declax[0]:
                                 line = "  // was STRUCT before --> " + declax[0] + "\n" + line
 
-                            fixed_classes[cls][func][i] = line
+                            self.fixed_classes[cls][func][i] = line
                             # print(cls + ":", "[fixing 2.1]", trace, line)
                     elif trace + " = " in line:
                         linx = line.lstrip()
@@ -86,7 +86,7 @@ class FunctionAnalyzer:
                             #    print(cls + ":", func, "[fixing 2.2]")
                             #    found_something = True
                             line = line.replace(linx + " = ", tracerx[trace] + "* " + trace + " = ") # make pointer for class for now!
-                            fixed_classes[cls][func][i] = line
+                            self.fixed_classes[cls][func][i] = line
                             # print(cls + ":", "[fixing 2.2]", trace, line)
         print("DONE")
 
@@ -94,18 +94,18 @@ class FunctionAnalyzer:
 
 
     def findOriginalClass(self, classes):
-        fixed_classes = dict()
+        self.fixed_classes = dict()
         print("Analyzing - finding original classes...")
 
         maxProcs = int(os.cpu_count() * 0.5) # 16 -> 8
         running_procs = []
         for i in range(maxProcs):
-            proc = mp.Process(target=self._help_find_original_class, args=(classes, fixed_classes, i, maxProcs))
+            proc = mp.Process(target=self._help_find_original_class, args=(classes, i, maxProcs))
             proc.start()
             running_procs.append(proc)
         for p in running_procs:
             p.join()
         print(f"Finished with {maxProcs} threads!")
 
-        return fixed_classes
+        return self.fixed_classes
 
