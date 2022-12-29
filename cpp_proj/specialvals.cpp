d80f56a61f251e0802ba5dfa6e7d41bcdd589eaa
Johandros <hansjuergoe@gmail.com>
2022-12-29 15:26:05 +0100

    processor count fix

--- cpp_proj/specialvals.cpp
+++ cpp_proj/specialvals.cpp
@@ -3,10 +3,10 @@
 uint processorCount()
 {
   uint processor_count = std::thread::hardware_concurrency();
-  if (processor_count > 1){
-    processor_count--;
-    if (processor_count > 4)
-      processor_count--;
-  }
+
+  processor_count *= 0.75;
+  if (processor_count <= 0)
+    processor_count = 1;
+
   return processor_count;
 }
