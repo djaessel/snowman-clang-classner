4b3944f530f312ca32ceda02c7e4abb6f9749a5d
Johandros <hansjuergoe@gmail.com>
2023-01-22 09:13:46 +0100

    run cpp classner from build folder script added

---
+++ run_cpp_classner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# FILESX=$(find . -maxdepth 1 -type f -exec grep "$@" {} +)
+# FILESX=$(find . -type f -exec grep "cpp_proj_run" {} +)
+# FILESX=$(ls | grep "cpp_proj_run")
+sudo updatedb
+FILESX=$(locate "cpp_proj_run" | grep $(pwd))
+
+# echo "$FILESX"
+echo "$FILESX $1"
+
