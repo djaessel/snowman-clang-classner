6d87574a9bb3e4b4ea9139068791d161ab22c351
Johandros <hansjuergoe@gmail.com>
2023-01-22 12:05:20 +0100

    run script update

--- run_cpp_classner.sh
+++ run_cpp_classner.sh
@@ -3,10 +3,11 @@
 # FILESX=$(find . -maxdepth 1 -type f -exec grep "$@" {} +)
 # FILESX=$(find . -type f -exec grep "cpp_proj_run" {} +)
 # FILESX=$(ls | grep "cpp_proj_run")
+
+# maybe not each time
 sudo updatedb
+
 FILESX=$(locate "cpp_proj_run" | grep $(pwd))
+# echo "$FILESX $@"
 
-# echo "$FILESX"
-echo "$FILESX $@"
 $FILESX $@
-
